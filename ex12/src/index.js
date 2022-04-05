8f55d3e46a168c7d1a474330819120b474b1dd8b
unknown <aisa.smajic27@gmail.com>
2022-04-05 23:50:02 +0200

    zadatak

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -2,7 +2,7 @@
 function logicOrder(num){
     if( num < 50 ){
         return "Less than 50";
-    }else if (num < 100){
+    } else if (num < 100){
         return "Less than 100";
     } else {
         return "Greater than or equal to 100";
