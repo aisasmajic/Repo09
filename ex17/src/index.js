f4998d9ee563e64d0f67a928006d46eadbe0b66d
unknown <aisa.smajic27@gmail.com>
2022-04-06 00:06:55 +0200

    zadatak

--- ex17/src/index.js
+++ ex17/src/index.js
@@ -1,14 +1,12 @@
 // Only change code below this line
 function inverseWhile() {
-    var fiveNumbers = "";
-    var i = 5;
-    while (i >= 0) {
-      i === 0 ? (fiveNumbers += i) : (fiveNumbers += i + ",");
-      i--;
-    }
-  
-    return fiveNumbers;
-  }
-  // Only change code above this line
-  console.log(inverseWhile());
-  module.exports = inverseWhile;
+  var fiveNumbers = "";
+  var i = 5;
+  while ( i >= 0) {
+  (i === 0)? fiveNumbers += i : fiveNumbers += i + ",";
+  i--;
+  } return fiveNumbers;
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
