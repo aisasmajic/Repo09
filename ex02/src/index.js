802a61fab748008777718a13f7052f2313a087ae
unknown <aisa.smajic27@gmail.com>
2022-04-05 10:32:41 +0200

    zadatak 2

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+function myFunction(myParameter) {
+   if(myParameter > 4) {
+        return  "The parameter is true!";
+    } 
+        return "The parameter is false!";
+    
+
+      
+
+    
+}
+console.log(myFunction((7)));
+console.log(myFunction((2)));
+
+// Only change code above this line
+module.exports = myFunction;
