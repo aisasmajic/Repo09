d55162bfbfc93cd131cad332e7b60c23aa25f743
unknown <aisa.smajic27@gmail.com>
2022-04-05 14:15:24 +0200

    ispravka

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -6,17 +6,19 @@ function comparisonToEqual(a){
     if (a < 10){
         return "Less than 10";
     }
-    if (a >= 10) {
-        return " 10 or over";
-    }
     if (a > 20) {
         return "More than 20";
     }
+    if (a >= 10) {
+        return " 10 or over";
+    }
+    
     
 }
      console.log(comparisonToEqual(0));
     console.log(comparisonToEqual(5));
     console.log(comparisonToEqual(17));
     console.log(comparisonToEqual(21));
+    
 // Only change code above this line
 module.exports = comparisonToEqual;
