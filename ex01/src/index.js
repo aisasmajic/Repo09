f5678c415d1e88c4091e400ac41307ee5de0c3d9
unknown <aisa.smajic27@gmail.com>
2022-04-05 09:48:18 +0200

    zadatak 1

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function myTrueBoolean(){
+    return Boolean(8 > 4);
+}
+console.log(myTrueBoolean());
+// Only change code above this line
+module.exports = myTrueBoolean;
